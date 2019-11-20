cf2091153fcbce96660b8c25261a6219a8ac2c2f
g-alcorn <griffin.alcorn@gmail.com>
2019-11-20 23:02:38 +0000

    finished

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,27 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.new do |s|
+  s.name = "burnaby"
+  s.annual_revenue = 3000000
+  s.womens_apparel = true
+  s.mens_apparel = true
+end
+burnaby.save
+
+richmond = Store.new do |s|
+  s.name = "richmond"
+  s.annual_revenue = 1260000
+  s.womens_apparel = true
+  s.mens_apparel = false
+end
+richmond.save
+
+gastown = Store.new do |s|
+  s.name = "gastown"
+  s.annual_revenue = 190000
+  s.womens_apparel = false
+  s.mens_apparel = true
+end
+gastown.save
+puts Store.count
